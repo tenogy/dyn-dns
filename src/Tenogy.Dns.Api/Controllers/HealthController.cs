24895681ea7cc013a5224eb46a45cdac24e8175c
Yuriy Korpachan <korpachan@gmail.com>
2020-03-21 13:53:27 +0300

    initialize app

---
+++ src/Tenogy.Dns.Api/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tenogy.Dns.Api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HealthController : ControllerBase
+	{
+		
+		[HttpGet]
+		public HealthState Get()
+		{
+			return new HealthState
+			{
+				Status = "healthy"
+			};
+		}
+	}
+
+	public class HealthState
+	{
+		public string Status { get; set; }
+	}
+}
