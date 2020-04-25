81317dfc645b7e493cde53c74e9ff4dbdaeb1d48
Yuriy Korpachan <korpachan@gmail.com>
2020-04-25 16:35:03 +0300

    fix deploy script

--- devops/scripts/deploy.sh
+++ devops/scripts/deploy.sh
@@ -2,7 +2,7 @@
 source $projectDir/devops/scripts/vars.sh;
 
 function deploy {
-    echo deploy start
+   
     readState
     echo "deploy the app ${appVersion}";
 
@@ -10,22 +10,20 @@ function deploy {
     sshC="-i $config_deploy_pk  $config_deploy_host";
     echo "ssh command: ${sshC}";
 
-    alias ssh-exec="ssh ${sshC}"
-
     echo "remove previous container versions"
-    ssh-exec "(if [ \$(docker images -q ${config_imageName}) ]; then docker rmi \$(docker images ${config_imageName} -q) -f; else echo /"No images for remove/";fi;);";
+    ssh ${sshC} "(if [ \$(docker images -q ${config_imageName}) ]; then docker rmi \$(docker images ${config_imageName} -q) -f; else echo /"No images for remove/";fi;);";
 
     echo save new image on remote server
     docker save $config_imageName:$appVersion | ssh -C $sshC docker load;
 
     echo stop app
-    ssh-exec "cd ${config_deploy_app} && docker-compose down -v --remove-orphans"
+    ssh ${sshC} "cd ${config_deploy_app} && docker-compose down -v --remove-orphans"
     echo copy docker-compose.yml
     scp -i $config_deploy_pk $projectDir/devops/docker-compose.yml $config_deploy_host:$config_deploy_app/docker-compose.yml
     echo fill environtment file
-    ssh-exec "cd ${config_deploy_app} && echo \"APP_VERSION=${appVersion}\" > .env && echo \"ASPNET_VERSION=${config_deploy_runtime}\" >> .env && echo \"APP_PORT=${config_deploy_port}\" >> .env"
+    ssh ${sshC} "cd ${config_deploy_app} && echo \"APP_VERSION=${appVersion}\" > .env && echo \"ASPNET_VERSION=${config_deploy_runtime}\" >> .env && echo \"APP_PORT=${config_deploy_port}\" >> .env"
     echo run app
-    ssh-exec "cd ${config_deploy_app} && docker-compose up -d"
+    ssh ${sshC} "cd ${config_deploy_app} && docker-compose up -d"
 
     echo docker prune
     docker system prune -f
